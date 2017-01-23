58808f1dd198115303c624058dcb5ea8f10570e5
dqiao <deeqiiao@gmail.com>
2017-01-22 20:33:28 -0600

    Changes to lightbulb code

--- lightbulb/Testing.java
+++ lightbulb/Testing.java
@@ -1,44 +1,50 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public static void main(String[] args){
-  int numLightBulbs = 0;
-  int numIteration = 0;
-  List<Integer> iterationPattern;
-  Scanner kbd = new Scanner(System.in);
+public class LightBulbs{
 
-  //Prompt the user for the # of light bulbs
-  System.out.println("Please enter the # of lightbulbs (0-20): ");
-  numLightBulbs = kbd.nextInt();
-  iterationPattern = new ArrayList<Integer>(numLightBulbs);
+  public static void main(String[] args){
+    int numLightBulbs = 0;
+    int numIteration = 0;
+    List<Integer> iterationPattern;
+    Scanner kbd = new Scanner(System.in);
 
-  //Prompt the user for the # of light bulb iterations
-  System.out.println("Please enter the # of lightbulbs to iterate thru: ");
-  numIteration = kbd.nextInt();
-
-  while(numLightBulbs >= 21){
-    System.out.println("Please enter a valid # of lightbulbs (0-20): ");
+    //Prompt the user for the # of light bulbs
+    System.out.println("Please enter the # of lightbulbs (0-20): ");
     numLightBulbs = kbd.nextInt();
     iterationPattern = new ArrayList<Integer>(numLightBulbs);
-  }
-  while(numIteration > numLightBulbs){
-    System.out.println("Please enter a valid # of lightbulbs to iterate thru: ");
+
+    //Prompt the user for the # of light bulb iterations
+    System.out.println("Please enter the # of lightbulbs to iterate thru: ");
     numIteration = kbd.nextInt();
-  }
 
-  for(int i = 1; i < iterationPattern.size()+1; i++){
-    int iterationPlace = i + numIteration;
-    boolean interationVal = false;
-
-    if(i == iterationPlace){
-      iterationVal = true;
-      array.put(i, iterationVal);
+    while(numLightBulbs >= 21){
+      System.out.println("Please enter a valid # of lightbulbs (0-20): ");
+      numLightBulbs = kbd.nextInt();
+      iterationPattern = new ArrayList<Integer>(numLightBulbs);
     }
-    else{
-      array.put(i, iterationVal);
+    while(numIteration > numLightBulbs){
+      System.out.println("Please enter a valid # of lightbulbs to iterate thru: ");
+      numIteration = kbd.nextInt();
     }
 
-  }
+    for(int i = 1; i < iterationPattern.size()+1; i++){
+      int iterationPlace = i + numIteration;
+      boolean iterationVal = false;
 
-  //Display the result here
-  System.out.println("Results below: ");
+      if(i == iterationPlace){
+        iterationVal = true;
+        iterationPattern.put(i, iterationVal);
+      }
+      else{
+        iterationPattern.put(i, iterationVal);
+      }
+
+    }
+
+    //Display the result here
+    System.out.println("Results below: ");
+  }
 }
